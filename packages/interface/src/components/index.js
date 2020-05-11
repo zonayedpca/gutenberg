bda1c79e6ad0f70a9ad2f636d1ae3ae951ea2686
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2020-05-11 18:24:28 +0200

    Interface: Introduce createFilteredComponent factory method

--- packages/interface/src/components/index.js
+++ packages/interface/src/components/index.js
@@ -1,5 +1,8 @@
 export { default as ComplementaryArea } from './complementary-area';
-export { default as FilteredComponent } from './filtered-component';
+export {
+	default as FilteredComponent,
+	createFilteredComponent,
+} from './filtered-component';
 export { default as FullscreenMode } from './fullscreen-mode';
 export { default as InterfaceSkeleton } from './interface-skeleton';
 export { default as PinnedItems } from './pinned-items';
