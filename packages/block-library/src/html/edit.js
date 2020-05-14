5469d5a8b69579644d21748a0ead0e563ce459ea
Zebulan Stanphill <zebulanstanphill@protonmail.com>
2020-05-14 14:55:05 -0500

    Polish Custom HTML block.

--- packages/block-library/src/html/edit.js
+++ packages/block-library/src/html/edit.js
@@ -1,119 +1,104 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 import {
+	__experimentalBlock as Block,
 	BlockControls,
 	PlainText,
 	transformStyles,
 } from '@wordpress/block-editor';
 import {
-	ToolbarButton,
 	Disabled,
 	SandBox,
+	ToolbarButton,
 	ToolbarGroup,
 } from '@wordpress/components';
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
-class HTMLEdit extends Component {
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			isPreview: false,
-			styles: [],
-		};
-		this.switchToHTML = this.switchToHTML.bind( this );
-		this.switchToPreview = this.switchToPreview.bind( this );
+// Default styles used to unset some of the styles
+// that might be inherited from the editor style.
+const DEFAULT_STYLES = `
+	html,body,:root {
+		margin: 0 !important;
+		padding: 0 !important;
+		overflow: visible !important;
+		min-height: auto !important;
+	}
+`;
+
+export default function HTMLEdit( { attributes, setAttributes, isSelected } ) {
+	const [ isPreview, setIsPreview ] = useState( false );
+	const [ styles, setStyles ] = useState( [] );
+
+	const editorStyles = useSelect(
+		( select ) => select( 'core/block-editor' ).getSettings().styles,
+		[]
+	);
+
+	useEffect( () => {
+		setStyles( [ DEFAULT_STYLES, ...transformStyles( editorStyles ) ] );
+	}, [ editorStyles ] );
+
+	function switchToPreview() {
+		setIsPreview( true );
 	}
 
-	componentDidMount() {
-		const { styles } = this.props;
-
-		// Default styles used to unset some of the styles
-		// that might be inherited from the editor style.
-		const defaultStyles = `
-			html,body,:root {
-				margin: 0 !important;
-				padding: 0 !important;
-				overflow: visible !important;
-				min-height: auto !important;
-			}
-		`;
-
-		this.setState( {
-			styles: [ defaultStyles, ...transformStyles( styles ) ],
-		} );
+	function switchToHTML() {
+		setIsPreview( false );
 	}
 
-	switchToPreview() {
-		this.setState( { isPreview: true } );
-	}
-
-	switchToHTML() {
-		this.setState( { isPreview: false } );
-	}
-
-	render() {
-		const { attributes, setAttributes } = this.props;
-		const { isPreview, styles } = this.state;
-
-		return (
-			<div className="wp-block-html">
-				<BlockControls>
-					<ToolbarGroup>
-						<ToolbarButton
-							className="components-tab-button"
-							isPressed={ ! isPreview }
-							onClick={ this.switchToHTML }
-						>
-							<span>HTML</span>
-						</ToolbarButton>
-						<ToolbarButton
-							className="components-tab-button"
-							isPressed={ isPreview }
-							onClick={ this.switchToPreview }
-						>
-							<span>{ __( 'Preview' ) }</span>
-						</ToolbarButton>
-					</ToolbarGroup>
-				</BlockControls>
-				<Disabled.Consumer>
-					{ ( isDisabled ) =>
-						isPreview || isDisabled ? (
-							<>
-								<SandBox
-									html={ attributes.content }
-									styles={ styles }
-								/>
-								{ /*	
-									An overlay is added when the block is not selected in order to register click events. 
-									Some browsers do not bubble up the clicks from the sandboxed iframe, which makes it 
-									difficult to reselect the block. 
-								*/ }
-								{ ! this.props.isSelected && (
-									<div className="block-library-html__preview-overlay"></div>
-								) }
-							</>
-						) : (
+	return (
+		<>
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton
+						isPressed={ ! isPreview }
+						onClick={ switchToHTML }
+					>
+						HTML
+					</ToolbarButton>
+					<ToolbarButton
+						isPressed={ isPreview }
+						onClick={ switchToPreview }
+					>
+						{ __( 'Preview' ) }
+					</ToolbarButton>
+				</ToolbarGroup>
+			</BlockControls>
+			<Disabled.Consumer>
+				{ ( isDisabled ) =>
+					isPreview || isDisabled ? (
+						<Block.div className="wp-block-html">
+							<SandBox
+								html={ attributes.content }
+								styles={ styles }
+							/>
+							{ /*
+								An overlay is added when the block is not selected in order to register click events.
+								Some browsers do not bubble up the clicks from the sandboxed iframe, which makes it
+								difficult to reselect the block.
+							*/ }
+							{ ! isSelected && (
+								<div className="block-library-html__preview-overlay" />
+							) }
+						</Block.div>
+					) : (
+						<Block.div className="wp-block-html is-html-editor">
 							<PlainText
+								__experimentalVersion={ 2 }
+								aria-label={ __( 'HTML' ) }
 								value={ attributes.content }
 								onChange={ ( content ) =>
 									setAttributes( { content } )
 								}
 								placeholder={ __( 'Write HTML…' ) }
-								aria-label={ __( 'HTML' ) }
 							/>
-						)
-					}
-				</Disabled.Consumer>
-			</div>
-		);
-	}
+						</Block.div>
+					)
+				}
+			</Disabled.Consumer>
+		</>
+	);
 }
-export default withSelect( ( select ) => {
-	const { getSettings } = select( 'core/block-editor' );
-	return {
-		styles: getSettings().styles,
-	};
-} )( HTMLEdit );
