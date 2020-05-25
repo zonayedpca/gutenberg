26feb8bab146b992806671ccba4afaac35cc0bff
Kelly Dwan <ryelle@users.noreply.github.com>
2020-05-25 19:19:24 -0400

    Block Directory: Add a "reinstall" button when a block type is not found

--- packages/block-directory/src/plugins/index.js
+++ packages/block-directory/src/plugins/index.js
@@ -2,14 +2,29 @@
  * WordPress dependencies
  */
 import { registerPlugin } from '@wordpress/plugins';
+import { addFilter } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
  */
 import InserterMenuDownloadableBlocksPanel from './inserter-menu-downloadable-blocks-panel';
+import missingInstallView from './missing-install-view';
 
 registerPlugin( 'block-directory', {
 	render() {
 		return <InserterMenuDownloadableBlocksPanel />;
 	},
 } );
+
+addFilter(
+	'blocks.registerBlockType',
+	'block-directory/fallback',
+	( settings, name ) => {
+		if ( name !== 'core/missing' ) {
+			return settings;
+		}
+		settings.edit = missingInstallView;
+
+		return settings;
+	}
+);
