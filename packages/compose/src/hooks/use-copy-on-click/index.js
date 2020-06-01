3e70409ec5fbb7e21a00b3bf03aeb1b1578c4b96
Jon Surrell <jon.surrell@automattic.com>
2020-06-01 23:39:51 +0200

    Type useCopyOnClick

--- packages/compose/src/hooks/use-copy-on-click/index.js
+++ packages/compose/src/hooks/use-copy-on-click/index.js
@@ -11,19 +11,21 @@ import { useRef, useEffect, useState } from '@wordpress/element';
 /**
  * Copies the text to the clipboard when the element is clicked.
  *
- * @param {Object}          ref     Reference with the element.
- * @param {string|Function} text    The text to copy.
- * @param {number}          timeout Optional timeout to reset the returned
- *                                  state. 4 seconds by default.
+ * @param {import('react').MutableRefObject<Element>} ref     Reference with the element.
+ * @param {string|Function}                        text    The text to copy.
+ * @param {number}                                 timeout Optional timeout to reset the returned
+ *                                                         state. 4 seconds by default.
  *
  * @return {boolean} Whether or not the text has been copied. Resets after the
  *                   timeout.
  */
 export default function useCopyOnClick( ref, text, timeout = 4000 ) {
+	/** @type {import('react').MutableRefObject<ClipboardJS|undefined>} */
 	const clipboard = useRef();
 	const [ hasCopied, setHasCopied ] = useState( false );
 
 	useEffect( () => {
+		/** @type {number|undefined} */
 		let timeoutId;
 
 		// Clipboard listens to click events.
@@ -32,24 +34,33 @@ export default function useCopyOnClick( ref, text, timeout = 4000 ) {
 			container: ref.current,
 		} );
 
-		clipboard.current.on( 'success', ( { clearSelection } ) => {
-			// Clearing selection will move focus back to the triggering button,
-			// ensuring that it is not reset to the body, and further that it is
-			// kept within the rendered node.
-			clearSelection();
+		/** @type {ClipboardJS} */ ( clipboard.current ).on(
+			'success',
+			( { clearSelection } ) => {
+				// Clearing selection will move focus back to the triggering button,
+				// ensuring that it is not reset to the body, and further that it is
+				// kept within the rendered node.
+				clearSelection();
 
-			if ( timeout ) {
-				setHasCopied( true );
-				clearTimeout( timeoutId );
-				timeoutId = setTimeout( () => setHasCopied( false ), timeout );
+				if ( timeout ) {
+					setHasCopied( true );
+					clearTimeout( timeoutId );
+					timeoutId = setTimeout(
+						() => setHasCopied( false ),
+						timeout
+					);
+				}
 			}
-		} );
+		);
 
 		return () => {
-			clipboard.current.destroy();
+			// eslint-disable-next-line no-unused-expressions
+			clipboard.current?.destroy();
 			clearTimeout( timeoutId );
 		};
 	}, [ text, timeout, setHasCopied ] );
 
 	return hasCopied;
 }
+
+/** @typedef {import('clipboard')} ClipboardJS */
