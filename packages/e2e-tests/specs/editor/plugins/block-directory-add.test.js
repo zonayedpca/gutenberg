e869633fb4b8ad212a0f1349218187a9623bd26d
dufresnesteven <steve.dufresne@automattic.com>
2020-07-24 15:01:00 +0900

    Make sure the results don't stay on the screen.

--- packages/e2e-tests/specs/editor/plugins/block-directory-add.test.js
+++ packages/e2e-tests/specs/editor/plugins/block-directory-add.test.js
@@ -192,7 +192,7 @@ describe( 'adding blocks from block directory', () => {
 
 		// The block will auto select and get added, make sure we see it in the content
 		expect( await getEditedPostContent() ).toMatchSnapshot();
-    } );
+	} );
 
 	it( 'Should not see Block Directory results after install', async () => {
 		// Setup our mocks
@@ -215,6 +215,5 @@ describe( 'adding blocks from block directory', () => {
 		);
 
 		expect( hiddenBtn ).toBeNull();
-
 	} );
 } );
