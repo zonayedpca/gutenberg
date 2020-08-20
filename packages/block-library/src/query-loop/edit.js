97c70385bcc1bae73196511478ced60b4ebecea8
ntsekouras <ntsekouras@outlook.com>
2020-08-20 16:30:39 +0300

    add basic order support in Query block

--- packages/block-library/src/query-loop/edit.js
+++ packages/block-library/src/query-loop/edit.js
@@ -19,7 +19,7 @@ const TEMPLATE = [ [ 'core/post-title' ], [ 'core/post-content' ] ];
 export default function QueryLoopEdit( {
 	clientId,
 	context: {
-		query: { perPage, offset, categoryIds },
+		query: { perPage, offset, categoryIds, order },
 		queryContext,
 	},
 } ) {
@@ -31,6 +31,7 @@ export default function QueryLoopEdit( {
 			const query = {
 				offset: perPage ? perPage * ( page - 1 ) + offset : 0,
 				categories: categoryIds,
+				order,
 			};
 			if ( perPage ) {
 				query.per_page = perPage;
@@ -44,7 +45,7 @@ export default function QueryLoopEdit( {
 				blocks: select( 'core/block-editor' ).getBlocks( clientId ),
 			};
 		},
-		[ perPage, page, offset, categoryIds, clientId ]
+		[ perPage, page, offset, categoryIds, order, clientId ]
 	);
 
 	const blockContexts = useMemo(
